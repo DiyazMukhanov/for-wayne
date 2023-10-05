b16c43337595ef39eb38517dcd35c5b39543c198
DiyazMukhanov <79104408+DiyazMukhanov@users.noreply.github.com>
2023-10-05 19:40:27 +0500

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -55,7 +55,7 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        //name of error CastError in case of getting tour with wrong id
+        
         let error = Object.create(err);
 
         if(err.name === 'CastError') error = handleCastErrorDB(error);
